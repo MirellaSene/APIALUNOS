23a0901d2d8eeef254d8afda0edab2dea885f846
MIRELLA DA SILVA SENE <25162437@edu119.local>
2025-08-22 16:04:46 -0300

    Adicionando post

--- server.js
+++ server.js
@@ -55,6 +55,7 @@ app.get("/alunos/:id", (req, res)=>{
 app.get("/alunos/:id", (req, res)=>{
 })
 
+
 app.get("/alunos/cor/:cor",(req, res)=>{
     const cor = req.params.cor.toLocaleLowerCase();
     console.log(`Cor recebida: ${cor}`);
@@ -62,6 +63,25 @@ app.get("/alunos/cor/:cor",(req, res)=>{
         (aluno) => aluno.cor.toLocaleLowerCase() === cor.toLowerCase()
     );
 })
+
+app.post("/alunos", (req, res)=>{
+    const {nome, cor, idade} = req.body;
+
+    if(!nome || !cor || !idade){
+        return res.status(400).json({msg : "Nome cor e idade são obrigatórios"})
+    }
+
+    const id = ALUNOS.length > 0 ? ALUNOS[ALUNOS.length - 1].id + 1 : 1
+
+    const novoAluno = {
+        id, nome, cor, idade
+    }
+    
+    console.log(novoAluno)
+    ALUNOS.push(novoAluno)
+    res.status(201).json({mensagem: "Aluno criado com sucesso"})
+})
+
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando em http://localhost:${PORT}`);
